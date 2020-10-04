0ea37e6ee392cb9d7d74011286da0efab6355256
so0choi <simc2644@gmail.com>
2020-10-04 14:23:53 +0900

    Did npm audit fix

--- App.js
+++ App.js
@@ -1,31 +1,32 @@
+import { WebView } from "react-native-webview";
+import React from "react";
+import { Vibration } from "react-native";
+import { Notifications } from "expo";
+import * as Permissions from "expo-permissions";
+import Constants from "expo-constants";
 
-import { WebView } from 'react-native-webview';
-import React from 'react';
-import {
-  Vibration
-} from 'react-native';
-import { Notifications } from 'expo'; 
-import * as Permissions from 'expo-permissions';   
-import Constants from 'expo-constants';
-
-const PUSH_REGISTRATION_ENDPOINT = 'http://133.186.150.88:3000/token';
+const PUSH_REGISTRATION_ENDPOINT = "http://13.125.159.57:3000/token";
 
 export default class App extends React.Component {
   state = {
     notification: null,
-    messageText: ''
-  }
-  tokenValue = '';
+    messageText: "",
+  };
+  tokenValue = "";
   registerForPushNotificationsAsync = async () => {
     if (Constants.isDevice) {
-      const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+      const { status: existingStatus } = await Permissions.getAsync(
+        Permissions.NOTIFICATIONS
+      );
       let finalStatus = existingStatus;
-      if (existingStatus !== 'granted') {
-        const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      if (existingStatus !== "granted") {
+        const { status } = await Permissions.askAsync(
+          Permissions.NOTIFICATIONS
+        );
         finalStatus = status;
       }
-      if (finalStatus !== 'granted') {
-        alert('푸시 알림이 기능이 해제됩니다');
+      if (finalStatus !== "granted") {
+        alert("푸시 알림이 기능이 해제됩니다");
         return;
       }
       token = await Notifications.getExpoPushTokenAsync();
@@ -33,46 +34,50 @@ export default class App extends React.Component {
       this.tokenValue = token;
       this.setState({ expoPushToken: token });
     } else {
-      alert('푸시 알림은 기기에서만 이용가능합니다.');
+      alert("푸시 알림은 기기에서만 이용가능합니다.");
     }
 
-    if (Platform.OS === 'android') {
-      Notifications.createChannelAndroidAsync('default', {
-        name: 'default',
+    if (Platform.OS === "android") {
+      Notifications.createChannelAndroidAsync("default", {
+        name: "default",
         sound: true,
-        priority: 'max',
+        priority: "max",
         vibrate: [0, 250, 250, 250],
       });
     }
 
     //node 서버로 토큰 전송
-    return fetch(PUSH_REGISTRATION_ENDPOINT,{
-      method:'POST',
+    return fetch(PUSH_REGISTRATION_ENDPOINT, {
+      method: "POST",
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        token: this.tokenValue
-      })
-    })
+        token: this.tokenValue,
+      }),
+    });
   };
 
-  componentDidMount(){
+  componentDidMount() {
     this.registerForPushNotificationsAsync();
-     this._notificationSubscription = Notifications.addListener(this._handleNotification);
+    this._notificationSubscription = Notifications.addListener(
+      this._handleNotification
+    );
   }
 
-  _handleNotification = notification => {
+  _handleNotification = (notification) => {
     Vibration.vibrate();
     console.log(notification);
-    this.setState({notification: notification});
-  }
+    this.setState({ notification: notification });
+  };
 
   render() {
-    return <WebView 
-    source={{ uri: 'http://133.186.150.88:3000/' }} 
-    style={{ paddingTop:20}} 
-    />;
-  } 
+    return (
+      <WebView
+        source={{ uri: "http://13.125.159.57:3000/" }}
+        style={{ paddingTop: 20 }}
+      />
+    );
+  }
 }
